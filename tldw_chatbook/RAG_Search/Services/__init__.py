d1342e351b52e6a4af0f7ae5f574bd410786ee07
Robert <contact@rmusser.net>
2025-06-22 18:55:12 -0700

    cleanup

--- tldw_chatbook/RAG_Search/Services/__init__.py
+++ tldw_chatbook/RAG_Search/Services/__init__.py
@@ -2,14 +2,6 @@
 # Description: RAG Services package initialization
 #
 # Imports
-from .embeddings_service import EmbeddingsService
-from .chunking_service import ChunkingService
-from .indexing_service import IndexingService
-from .cache_service import CacheService, get_cache_service
-from .memory_management_service import MemoryManagementService, MemoryManagementConfig
-from .config_integration import get_rag_config, get_memory_config, save_rag_setting, get_rag_setting
-from .service_factory import RAGServiceFactory, create_rag_services, create_embeddings_service, create_indexing_service, create_modular_rag_service
-
 # Import the new modular RAG service
 try:
     from .rag_service.integration import RAGService
