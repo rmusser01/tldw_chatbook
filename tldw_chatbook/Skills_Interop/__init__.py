573ee3632732f226033fe9711b91c7741eff03a7
Robert <contact@rmusser.net>
2026-05-01 11:07:44 -0700

    feat: add local skills service

--- tldw_chatbook/Skills_Interop/__init__.py
+++ tldw_chatbook/Skills_Interop/__init__.py
@@ -1,6 +1,7 @@
-"""Remote server-skill interoperability services."""
+"""Local and server SKILL.md interoperability services."""
 
+from .local_skills_service import LocalSkillsService
 from .server_skills_service import ServerSkillsService
 from .skills_scope_service import SkillsBackend, SkillsScopeService
 
-__all__ = ["ServerSkillsService", "SkillsBackend", "SkillsScopeService"]
+__all__ = ["LocalSkillsService", "ServerSkillsService", "SkillsBackend", "SkillsScopeService"]
